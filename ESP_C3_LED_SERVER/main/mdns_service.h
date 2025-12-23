780eb2ce29ebde668b240c42e933c7373e04d4c0
Adrian Rojek <arojek.work@gmail.com>
2025-12-23 13:52:52 +0100

    Moved to separate repo

--- ESP_C3_LED_SERVER/main/mdns_service.h
+++
@@ -1,9 +0,0 @@
-#ifndef MDNS_SERVICE_H
-#define MDNS_SERVICE_H
-
-#include "esp_err.h"
-
-esp_err_t mdns_service_init(const char* device_name);
-void mdns_service_stop(void);
-
-#endif // MDNS_SERVICE_H
